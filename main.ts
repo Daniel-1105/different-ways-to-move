acfe153489f2ba7c90aed656fe19c0430d9be622
Daniel-1105 <82467131+Daniel-1105@users.noreply.github.com>
2021-04-15 13:47:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+scene.setBackgroundColor(1)
+let Dog = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+Dog.setStayInScreen(true)
+game.onUpdate(function () {
+    controller.moveSprite(Dog, 100, 100)
+})
